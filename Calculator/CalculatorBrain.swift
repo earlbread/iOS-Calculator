51f1d615e8910b4702ccda5e53842f12e54fd091
Seunghun Lee <waydi1@gmail.com>
2017-06-02 01:29:45 +0900

    Refactor performOperation with enum and Dictionary

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,16 +11,26 @@ import Foundation
 struct CalculatorBrain {
     private var accumulator: Double?
 
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+    }
+
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "√": Operation.unaryOperation(sqrt),
+    ]
+
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
             }
-        default:
-            break
         }
     }
 
