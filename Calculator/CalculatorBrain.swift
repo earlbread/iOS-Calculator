571ec247364d1c32caf36f2415d710d6dd997259
Seunghun Lee <waydi1@gmail.com>
2017-06-02 23:46:09 +0900

    Add the binary operation

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -14,11 +14,15 @@ struct CalculatorBrain {
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
     }
 
     private var operations: Dictionary<String, Operation> = [
         "π": Operation.constant(Double.pi),
         "√": Operation.unaryOperation(sqrt),
+        "×": Operation.binaryOperation({ $0 * $1 }),
+        "=": Operation.equals
     ]
 
     mutating func performOperation(_ symbol: String) {
@@ -30,7 +34,32 @@ struct CalculatorBrain {
                 if accumulator != nil {
                     accumulator = function(accumulator!)
                 }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
             }
+        }
+    }
+
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+
+    private var pendingBinaryOperation: PendingBinaryOperation?
+
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
         }
     }
 
